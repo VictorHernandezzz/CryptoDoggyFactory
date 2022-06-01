ed576aad3fc79313742dce24157946efc8ee6aed
Bikutā ビクター <anthosedits@gmail.com>
2022-06-01 12:56:07 -0400

    added random button, so now user can randomly create a doggy based all the colors and attributes

--- client/assets/js/buttons.js
+++ client/assets/js/buttons.js
@@ -16,6 +16,43 @@ async function showAttributesMenu() {
     $('.dogColorsClass').css('display', 'none')
 }
 
+//Randomize Function
+$('#random').click(()=>{
+    var skycolor = Math.floor(Math.random() * 89) + 10;
+    skyColor(colors[skycolor],skycolor)
+    $('#skyColor').val(skycolor)
+    var bubblecolor = Math.floor(Math.random() * 89) + 10;
+    bubbleColor(colors[bubblecolor],bubblecolor)
+    $('#bubbleColor').val(bubblecolor)
+    var bodycolor = Math.floor(Math.random() * 89) + 10;
+    headColor(colors[bodycolor],bodycolor)
+    $("#bodycolor").val(bodycolor)
+    var mouthcolor = Math.floor(Math.random() * 89) + 10;
+    mouthColor(colors[mouthcolor],mouthcolor)
+    $("#mouthColor").val(mouthcolor)
+    var eyescolor = Math.floor(Math.random() * 89) + 10;
+    eyesColor(colors[eyescolor],eyescolor)
+    $("#eyesColor").val(eyescolor)
+    var earscolor = Math.floor(Math.random() * 89) + 10;
+    earsColor(colors[earscolor],earscolor)
+    $("#earsColor").val(earscolor)
+    var eyevar = Math.floor(Math.random() * (7 - 1 + 1) + 1);
+    eyeVariation(eyevar)
+    $("#eyeshape").val(eyevar)
+    var decovar = Math.floor(Math.random() * (5 - 1 + 1) + 1);
+    decorationVariation(decovar)
+    $("#decorationstyle").val(decovar)
+    var decMidVar = Math.floor(Math.random() * 89) + 10;
+    decorationMidColorVar(colors[decMidVar],decMidVar)
+    $("#decMidColor").val(decMidVar)
+    var decSideVar = Math.floor(Math.random() * 89) + 10;
+    decorationSidesColorVar(colors[decSideVar],decSideVar)
+    $("#decSideColor").val(decSideVar)
+    var anim = Math.floor(Math.random() * (3 - 1 + 1) + 1);
+    animationsPlayer(anim)
+    $("#animation").val(anim)
+  })
+
 //button for default NFT
 $('#reset').click(()=>{ 
 
